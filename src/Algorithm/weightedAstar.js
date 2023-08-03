a118ccafa20de5f36543a8525fe1ed0eaa0f3e28
Yi Cheng Lee <leeyicheng10@gmail.com>
2023-08-03 23:27:12 +0800

    fixed weighted A*

--- src/Algorithm/weightedAstar.js
+++ src/Algorithm/weightedAstar.js
@@ -44,7 +44,7 @@ const getUnvisitedNeighbours = (node, grid, startNode, endNode) => {
   for (const neighbour of filteredNeighbours) {
     neighbour.g_cost = getG_Cost(neighbour, grid, endNode);
     neighbour.h_cost = getH_Cost(neighbour, grid, startNode);
-    neighbour.f_cost = neighbour.g_cost + 0.3 * neighbour.h_cost;
+    neighbour.f_cost = neighbour.g_cost + 2 * neighbour.h_cost;
     neighbour.previousNode = node;
   }
   return filteredNeighbours;
